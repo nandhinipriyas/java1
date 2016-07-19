fcceb2f27b2b18d2cb8ca6682b35aa8aabf8c155
nandhinipriyas <npriya163@gmail.com>
2016-07-19 10:11:47 +0530

    Update and rename SAMPLE1.java to SAMPLE.java

---
+++ SAMPLE.java
@@ -0,0 +1,27 @@
+
+public class SAMPLE{
+
+
+	 	 
+	public static void main(String[] args) {
+		if(args.length==0)
+		{
+			System.out.println("please enter the integer");
+		}
+		else{
+			int a=Integer.parseInt(args[0]);
+		
+			if(a>0)
+			{
+				System.out.println(""+a+" is positive number");
+				
+			}
+		else if(a==0)
+	{
+System.out.println(""+a+" is neither positive nor negative number");
+}
+		else{
+			System.out.println(""+a+" is negative number");
+		}
+}
+}}
